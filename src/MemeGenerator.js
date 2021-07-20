f45999586af439cc9d8a7efa3bc2892e7494f9fb
Micah Sterling <msterling89@gmail.com>
2021-07-19 20:44:55 -0500

    add joke api

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -11,19 +11,33 @@ class MemeGenerator extends Component {
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.getJoke = this.getJoke.bind(this)
   }
   handleChange(event) {
     const { name, value } = event.target
     this.setState({ [name]: value })
     console.log(name, value)
   }
+  getJoke() {
+    // make api call
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then(response => response.json())
+      .then(response => {
+        // set to top text 
+        // set to bottom text 
+        this.setState({ topText: response.setup, bottomText: response.punchline })
+        console.log(response.setup)
+      })
+  }
   handleSubmit(event) {
     event.preventDefault()
     const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
     const randMemeImg = this.state.allMemeImgs[randNum].url
     this.setState({ randomImg: randMemeImg })
+    this.getJoke()
   }
   componentDidMount() {
+    this.getJoke()
     fetch("https://api.imgflip.com/get_memes")
       .then(response => response.json())
       .then(response => {
